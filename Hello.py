3d74bdfe1b6bd161ca3ccb556f1ae85c7c25b3c4
abdsaeed92 <rahmansaeed1992@gmail.com>
2023-12-04 20:52:45 +0300

    Hello python  file

---
+++ Hello.py
@@ -0,0 +1,14 @@
+# printing in Python
+print("Hello, Python!")
+
+# solving simple equations
+a = 1
+b = 5
+c = a+b
+
+print(c)
+
+# loops
+nation = ["India", "Philippines", "Pakistan"]
+for x in nation:
+  print(x)
